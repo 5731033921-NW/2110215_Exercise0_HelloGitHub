c9442968ca4db231000542da7f23038b9492be95
fifap_000 <fifap_000@fifapesrf>
2016-01-21 16:56:31 +0700

    firstpull

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -21,19 +21,18 @@ public class Hello {
 	public static int[] bubbleSort(int[] array){
 		// TODO Implement this method to sort the given array in an ascending order and return the sorted array.
 		
-		Boolean IsSwap;
-		do{
-			IsSwap = false;
+		Boolean swap = true;
+		while(swap){
+			swap = false;
 			for (int i=0; i<array.length -1;i++)
 			{	if (array[i]>array[i+1])
 				{	int temp = array[i];
 					 array[i] = array[i+1];	
 					 array[i+1] = temp;		 
-					 IsSwap = true;
+					 swap = true;
 				}
 			}
 		}
-		while(IsSwap);
 		return array;
 	}
 	
